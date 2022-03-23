b87c443f2a0e0b14ae3eee662b6b041b1a864c2e
Ryan Segura <>
2022-03-23 01:21:30 -0500

    added who its for

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,13 +8,131 @@
         <v-img src="/img/hero-01.png"></v-img>
       </v-col>
     </v-row>
+    <v-row class="mt-16">
+      <v-col class="text-h3 text-center"> WHO IS NFTY-STAYS FOR? </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" xl="3">
+        <v-card class="mx-auto rounded-xl" color="amber darken-2 elevation-7">
+          <v-img src="/img/vr-2.jpg" height="200" class=""></v-img>
+          <v-card-actions
+            ><v-card-title>Investors</v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              large
+              @click="isInvestorExpandShowing = !isInvestorExpandShowing"
+              >Explore
+              <v-icon class="ml-2">{{
+                isInvestorExpandShowing ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <div v-show="isInvestorExpandShowing">
+              <v-divider></v-divider>
+              <v-card-text>
+                Future Monetization and Tokenization Projects
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" xl="3">
+        <v-card class="mx-auto rounded-xl" color="lime lighten-2">
+          <v-img src="/img/vr-4.jpg" height="200" class="align-end"></v-img>
+          <v-card-actions
+            ><v-card-title>Host</v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              large
+              @click="isHostExpandShowing = !isHostExpandShowing"
+              >Explore
+              <v-icon class="ml-2">{{
+                isHostExpandShowing ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <div v-show="isHostExpandShowing">
+              <v-divider></v-divider>
+              <v-card-text>
+                Future Franchisee opportunities to increase demand and ROI
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" xl="3">
+        <v-card class="mx-auto rounded-xl" color="light-blue lighten-2">
+          <v-img src="/img/vr-3.jpg" height="200"></v-img>
+          <v-card-actions
+            ><v-card-title>Guests</v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              large
+              @click="isGuestExpandShowing = !isGuestExpandShowing"
+              >Explore
+              <v-icon class="ml-2">{{
+                isGuestExpandShowing ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <div v-show="isGuestExpandShowing">
+              <v-divider></v-divider>
+              <v-card-text>
+                All Travelers & Guest will love our experiences. Web3/NFT
+                Newcomers and Vets alike. Builders, Investors, and Digital
+                Nomads. escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" xl="3">
+        <v-card class="mx-auto rounded-xl" color="pink lighten-4">
+          <v-img src="/img/vr-1.jpg" height="200"></v-img>
+          <v-card-actions
+            ><v-card-title>Real Estate</v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              large
+              @click="isRealEstateExpandShowing = !isRealEstateExpandShowing"
+              >Explore
+              <v-icon class="ml-2">{{
+                isRealEstateExpandShowing
+                  ? "mdi-chevron-up"
+                  : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <div v-show="isRealEstateExpandShowing">
+              <v-divider></v-divider>
+              <v-card-text
+                >Project STOIC- Real Estate Education and Investment NFT Project
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "home-view",
-
+  data: () => ({
+    isHostExpandShowing: false,
+    isInvestorExpandShowing: false,
+    isGuestExpandShowing: false,
+    isRealEstateExpandShowing: false,
+  }),
   components: {},
 };
 </script>
