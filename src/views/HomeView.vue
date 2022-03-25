6f8e9993bf7b37b1735ef2b1f3b47ee165da9875
Ryan Segura <>
2022-03-24 22:52:01 -0500

    Added remaining sections.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,9 @@
       </v-col>
     </v-row>
     <v-row class="mt-12">
-      <v-col class="text-h3 text-center"> WHO IS NFTY-STAYS FOR? </v-col>
+      <v-col class="text-h3 text-center lime--text text--lighten-2">
+        WHO IS NFTY-STAYS FOR?
+      </v-col>
     </v-row>
     <v-row>
       <v-col cols="12" sm="6" xl="3">
@@ -122,11 +124,15 @@
       </v-col>
     </v-row>
     <v-row class="mt-16">
-      <v-col class="text-h3 text-center"> NFTY STAYS MISSION </v-col>
+      <v-col class="text-h3 text-center light-blue--text text--lighten-2">
+        NFTY STAYS MISSION
+      </v-col>
     </v-row>
-    <v-row class="d-flex flex-wrap justify-center">
+    <v-row
+      class="d-flex flex-wrap justify-center align-center indigo darken-3 pa-12 rounded-xl"
+    >
       <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-card color="lime lighten-2" class="black--text" dark>
+        <v-card color="lime lighten-2" class="black--text rounded-xl" dark>
           <v-card-title class="text-h5"> Immersion </v-card-title>
           <v-card-subtitle class="black--text"
             >Immerse guest in web3 and NFT experiences</v-card-subtitle
@@ -134,7 +140,7 @@
         </v-card>
       </v-col>
       <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-card color="pink lighten-4" class="black--text" dark>
+        <v-card color="pink lighten-4" class="black--text rounded-xl" dark>
           <v-card-title class="text-h5"> Education </v-card-title>
           <v-card-subtitle class="black--text"
             >Onboard new Travelers, Host, and RE Investors to Web3 and
@@ -143,7 +149,11 @@
         </v-card>
       </v-col>
       <v-col cols="12" sm="6" md="4" lg="3" xl="3">
-        <v-card color="light-blue lighten-2" class="black--text" dark>
+        <v-card
+          color="light-blue lighten-2"
+          class="black--text rounded-xl"
+          dark
+        >
           <v-card-title class="text-h5"> Immersion </v-card-title>
           <v-card-subtitle class="black--text"
             >Build a community of Short Term Rental and Real Estate investors
@@ -152,7 +162,7 @@
         </v-card>
       </v-col>
       <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-card color="pink lighten-4" class="black--text" dark>
+        <v-card color="pink lighten-4" class="black--text rounded-xl" dark>
           <v-card-title class="text-h5"> Yield </v-card-title>
           <v-card-subtitle class="black--text"
             >Develop ancillary income streams for independent
@@ -161,12 +171,110 @@
         </v-card>
       </v-col>
       <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-        <v-card color="lime lighten-2" class="black--text" dark>
+        <v-card color="lime lighten-2" class="black--text rounded-xl" dark>
           <v-card-title class="text-h5"> Adoption </v-card-title>
           <v-card-subtitle class="black--text"
             >Be the early adopters that set industry standards</v-card-subtitle
           >
         </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="mt-16">
+      <v-col class="text-h3 text-center pink--text text--accent-3">
+        FIRST PROOF OF CONCEPT
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-img src="/img/booshie.jpg" class="rounded-xl"></v-img>
+      </v-col>
+      <v-col cols="12" sm="6"
+        ><v-card
+          color="lime lighten-2 mb-6"
+          class="black--text rounded-xl"
+          dark
+        >
+          <v-card-title class="text-h5 mb-1">
+            Booshie's Crypto Crib
+          </v-card-title>
+          <v-card-subtitle class="black--text">
+            <ul>
+              <li>3 bedroom, 1 bathroom Stay</li>
+              <li>Performing Short Term Rental</li>
+              <li>Operated since 2019 as Lily’s Pad JAX</li>
+              <li>Will start rebrand and concept deployment on next vacancy</li>
+              <li>April 22</li>
+              <li>Booshies CRYPTO PAD</li>
+              <li>Be the early adopters that set industry standards</li>
+            </ul>
+          </v-card-subtitle> </v-card
+        ><iframe
+          class="rumble rounded-xl"
+          width="100%"
+          height="300"
+          src="https://rumble.com/embed/vd5wp3/?pub=4"
+          frameborder="0"
+          allowfullscreen
+        ></iframe
+      ></v-col>
+    </v-row>
+    <v-row class="mt-16">
+      <v-col class="text-h3 text-center light-blue--text text--lighten-2">
+        THE NEXT NFTY STAYS
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-img src="/img/fl-basement.png"></v-img>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-img src="/img/1-bed-1-bath.png"></v-img>
+      </v-col>
+    </v-row>
+    <v-row class="mt-12">
+      <v-col class="text-h3 text-center orange--text text--lighten-1">
+        JUST SCRATCHING THE SURFACE
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        ><v-card color="pink lighten-4" class="black--text rounded-xl" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="text-h5"> Education </v-card-title>
+              <v-card-subtitle class="black--text">
+                Web3 ecosystem for STR</v-card-subtitle
+              >
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img src="/img/nft-1.jpg"></v-img>
+            </v-avatar>
+          </div> </v-card
+      ></v-col>
+    </v-row>
+    <v-row class="mt-12">
+      <v-col class="text-h3 text-center lime--text text--lighten-2">
+        FOUNDER
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" class="d-flex align-center text-h5">
+        <div>
+          <strong>Joshua DeDecker AKA reconwhispers</strong>
+          <ul>
+            <li>RealEstateInvestor</li>
+            <li>Real Estate Operations Specialist</li>
+            <li>Founder & CEO of Patriot Property Collective</li>
+            <li>
+              Founder of
+              <a href="https://www.dig-a-hash.com" target="_new">Dig-A-Hash</a>
+            </li>
+          </ul>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-img src="/img/founder.png"></v-img>
       </v-col>
     </v-row>
   </v-container>
